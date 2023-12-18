6fd9d4e7011e83d1c6ab44718edbb499c7323f7c
Parth Saini <parthsaini13@gmail.com>
2023-12-18 23:14:10 +0530

    added sort

--- script.js
+++ script.js
@@ -13,3 +13,47 @@ if(close) {
     nav.classList.remove('active');
   })
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const productContainer = document.querySelector(".pro-container");
+  const products = Array.from(document.querySelectorAll(".pro"));
+
+  
+  const sortByPrice = (order) => {
+    products.sort((a, b) => {
+      const priceA = parseFloat(a.querySelector("h4").innerText.slice(1));
+      const priceB = parseFloat(b.querySelector("h4").innerText.slice(1));
+
+      if (order === "lowToHigh") {
+        return priceA - priceB;
+      } else if (order === "highToLow") {
+        return priceB - priceA;
+      }
+    });
+
+    
+    productContainer.innerHTML = "";
+
+    
+    products.forEach((product) => {
+      productContainer.appendChild(product);
+    });
+  };
+
+ 
+  const sortPrices = () => {
+    const sortOrderSelect = document.getElementById("sortOrder");
+    const selectedOrder = sortOrderSelect.value;
+    sortByPrice(selectedOrder);
+  };
+
+  
+  document.getElementById("sortOrder").addEventListener("change", sortPrices);
+
+  
+  document.querySelector(".btne").addEventListener("click", () => {
+    const sortOrderSelect = document.getElementById("sortOrder");
+    const selectedOrder = sortOrderSelect.value;
+    sortByPrice(selectedOrder);
+  });
+});
