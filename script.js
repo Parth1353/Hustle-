4008b5acab7301038c9f9baca470247214e1ec3a
Parth Saini <parthsaini13@gmail.com>
2023-12-18 23:38:57 +0530

    modal added

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
-
-if(bar) {
-  bar.addEventListener('click', () => {
-    nav.classList.add('active');
-  })
-};
-
-if(close) {
-  close.addEventListener('click', () => {
-    nav.classList.remove('active');
-  })
-};
-
 document.addEventListener("DOMContentLoaded", function () {
   const productContainer = document.querySelector(".pro-container");
   const products = Array.from(document.querySelectorAll(".pro"));
 
-  
   const sortByPrice = (order) => {
     products.sort((a, b) => {
       const priceA = parseFloat(a.querySelector("h4").innerText.slice(1));
@@ -31,29 +14,54 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-    
     productContainer.innerHTML = "";
 
-    
     products.forEach((product) => {
       productContainer.appendChild(product);
     });
   };
 
- 
   const sortPrices = () => {
     const sortOrderSelect = document.getElementById("sortOrder");
     const selectedOrder = sortOrderSelect.value;
     sortByPrice(selectedOrder);
   };
 
-  
   document.getElementById("sortOrder").addEventListener("change", sortPrices);
 
-  
-  document.querySelector(".btne").addEventListener("click", () => {
-    const sortOrderSelect = document.getElementById("sortOrder");
-    const selectedOrder = sortOrderSelect.value;
-    sortByPrice(selectedOrder);
+  // Updated function to handle "Add to Cart" button click
+  const openModal = () => {
+    modal.style.display = "block";
+    document.body.style.overflow = "hidden"; // Prevent scrolling on the body
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    modal.style.display = "none";
+    document.body.style.overflow = ""; // Restore scrolling on the body
+  };
+
+  // Event listener for the "Add to Cart" button
+  products.forEach((product) => {
+    product.querySelector(".cart").addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent the default behavior (scrolling to the top)
+      openModal();
+    });
+  });
+
+  // Your existing modal code
+  var modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("close")[0];
+
+  // Event listener to close the modal when the close button is clicked
+  span.addEventListener("click", () => {
+    closeModal();
+  });
+
+  // Event listener to close the modal when clicking outside the modal
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      closeModal();
+    }
   });
 });
